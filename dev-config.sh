4498e73b1203e9557c705e3c0e020de5deef0203
Baraian Camelia <camelia.baraian@rover.com>
2022-11-23 16:25:06 +0200

    add env variables

--- dev-config.sh
+++
@@ -1,5 +0,0 @@
-export DATABASE_NAME="django"
-# export DATABASE_USER="camelia.baraian"
-export DATABASE_PORT="5432"
-
-unset DJANGO_DEBUG
--- staging-config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export DJANGO_DEBUG=0
-export DATABASE_NAME="staging"
-# export DATABASE_USER="camelia.baraian"
-export DATABASE_PORT="5432"
