73bb27520b82013febf11ad28ebc5e5e75386dd7
Baraian Camelia <camelia.baraian@rover.com>
2022-11-29 15:45:55 +0200

    update test with full_clean() validation

--- customers/tests/customers_tests.py
+++ customers/tests/customers_tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from customers.models import Person, Provider, Pet
 from .factories import PersonFactory, ProviderFactory, PetFactory
+from django.core.exceptions import ValidationError
 
 
 class PersonTestCase(TestCase):
@@ -8,14 +9,29 @@ class PersonTestCase(TestCase):
         person1 = PersonFactory.create()
         self.assertIn(person1, Person.objects.all())
 
+        try:
+            person1.full_clean()
+        except ValidationError as e:
+            self.assertRaises('name' in e.message_dict)
+
 
 class ProviderTestCase(TestCase):
     def test_provider(self):
         provider1 = ProviderFactory.create()
         self.assertIn(provider1, Provider.objects.all())
 
+        try:
+            provider1.full_clean()
+        except ValidationError as e:
+            self.assertRaises('name' in e.message_dict)
+
 
 class PetTestCase(TestCase):
     def test_pet(self):
         pet1 = PetFactory.create()
         self.assertIn(pet1, Pet.objects.all())
+
+        try:
+            pet1.full_clean()
+        except ValidationError as e:
+            self.assertRaises('name' in e.message_dict)
