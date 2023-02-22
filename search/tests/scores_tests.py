6ddddef7f5d28299e606731ac52e9c026722876f
Baraian Camelia <camelia.baraian@rover.com>
2023-02-22 19:05:13 +0200

    add tests

--- search/tests/scores_tests.py
+++ search/tests/scores_tests.py
@@ -4,35 +4,40 @@ from customers.tests.factories import ProviderFactory, PersonFactory
 from stays.tests.factories import StayFactory, ReviewFactory
 from unittest.mock import patch
 from ..scoring import compute_sitter_score, compute_ratings_score
+from customers.models import Person
 
 
 class SearchScoreTestCase(TestCase):
+    def setUp(self):
+        self.person = PersonFactory.create()
+        self.provider = ProviderFactory.create(person=self.person)
+
     def test_create_search_score_on_provider_created(self):
-        provider = ProviderFactory.create()
-        self.assertIn(provider.search_score, SearchScore.objects.all())
+        self.assertIn(self.provider.search_score, SearchScore.objects.all())
 
     def test_sitter_score_and_overall_rank_update_when_person_name_changes(self):
-        person = PersonFactory.create()
-        provider = ProviderFactory.create(person=person)
-        person_name = person.name
-        provider_sitter_score = provider.search_score.sitter_score
-        provider_overall_rank = provider.search_score.overall_rank
-        person.name = 'Mariana'
-        person.save()
-        person.refresh_from_db()
-
-        self.assertNotEqual(person_name, person.name)
-        self.assertNotEqual(provider_sitter_score, provider.search_score.sitter_score)
-        self.assertNotEqual(provider_overall_rank, provider.search_score.overall_rank)
+        person_name = self.person.name
+        provider_sitter_score = self.provider.search_score.sitter_score
+        provider_overall_rank = self.provider.search_score.overall_rank
+        self.person.name = 'Mariana'
+        self.person.save()
+        self.person.refresh_from_db()
+        self.assertNotEqual(person_name, self.person.name)
+        self.assertNotEqual(provider_sitter_score, self.provider.search_score.sitter_score)
+        self.assertNotEqual(provider_overall_rank, self.provider.search_score.overall_rank)
 
     def test_rating_score_is_computed_when_review_is_added(self):
-        provider = ProviderFactory.create()
-        stay = StayFactory.create(provider=provider)
+        stay = StayFactory.create(provider=self.provider)
         review = ReviewFactory.create(stay=stay)
-        rating_score = compute_ratings_score(provider)
+        rating_score = compute_ratings_score(self.provider)
         review.save()
         review.refresh_from_db()
+        self.assertEqual(rating_score, self.provider.search_score.rating_score)
 
-        self.assertEqual(rating_score, provider.search_score.rating_score)
+    def test_rating_score_is_not_computed_when_review_is_missing(self):
+        stay = StayFactory.create(provider=self.provider)
+        self.assertEqual(self.provider.search_score.rating_score, 0)
+        
+
    
  
