2d753bfa369226ca19bc4200045fd4b573ed5709
Baraian Camelia <camelia.baraian@rover.com>
2023-03-28 12:54:37 +0300

    add tests

--- api/api_tests/api_tests.py
+++ api/api_tests/api_tests.py
@@ -8,9 +8,27 @@ from api.views import ProviderListViewSet
 
 
 class ProviderApiTests(APITestCase):
-    def test_get_provider(self):
+    def test_retrieve_provider(self):
         provider = ProviderFactory.create()
         client =APIClient()
         request = client.get('/api/provider/' + str(provider.id) + '/') 
         response = ProviderListViewSet.retrieve(self, request, pk=provider.id)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_retrieve_one_provider(self):
+        provider = ProviderFactory.create()
+        client =APIClient()
+        request = client.get('/api/provider/' + str(provider.id) + '/') 
+        response = ProviderListViewSet.retrieve(self, request, pk=provider.id)
+        self.assertEqual(response.data['name'], 'Jhon')
+
+    def test_return_404_when_id_does_not_exists(self):
+        client =APIClient()
+        request = client.get('/api/provider/0/') 
+        self.assertEqual(request.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_list_of_providers(self):
+        client = APIClient()
+        request = client.get('/api/provider/')
+        response = ProviderListViewSet.list(self, request)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
