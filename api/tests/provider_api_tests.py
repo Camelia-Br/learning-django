3c703fc56c7dd79a10cfbdc87edc373930261735
Baraian Camelia <camelia.baraian@rover.com>
2023-04-04 15:44:49 +0300

    update view set and tests

--- api/tests/provider_api_tests.py
+++ api/tests/provider_api_tests.py
@@ -6,40 +6,37 @@ from customers.tests.factories import ProviderFactory
 from rest_framework.test import APIClient
 from api.views import ProviderListViewSet
 from django.http.response import Http404
-from api.serializers import ProviderSerializer
+from django.test import RequestFactory
 
 
-class ProviderApiRetrieveTests(APITestCase):
+class BaseAPITest(APITestCase):
     def setUp(self):
+        self.factory = RequestFactory()
         self.client = APIClient()
+        self.provider = ProviderFactory.create()
+        self.provider1 = ProviderFactory.create()
+        self.provider2 = ProviderFactory.create()
+        self.view_set = ProviderListViewSet
 
+
+class ProviderApiRetrieveTests(BaseAPITest):
     def test_retrieve_provider(self):
-        provider = ProviderFactory.create()
-        request = self.client.get('/api/providers/' + str(provider.id) + '/')
-        response = ProviderListViewSet.retrieve(self, request, pk=provider.id)
+        request = self.factory.get(f'/api/providers/{str(self.provider.person.id)}/')
+        response = self.view_set.as_view({'get': 'retrieve'})(request, pk=str(self.provider.person.id))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['name'], provider.person.name)
+        self.assertEqual(response.data['name'], self.provider.person.name)
 
     def test_return_404_when_id_does_not_exists(self):
-        request = self.client.get('/api/providers/0/')
-        with self.assertRaises(Http404):
-            ProviderListViewSet.retrieve(self, request, pk='0')
+        request = self.factory.get('/api/providers/0/')
+        response = self.view_set.as_view({'get': 'retrieve'})(request, pk='0')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 
-class ProviderApiListTests(APITestCase):
+class ProviderApiListTests(BaseAPITest):
     def test_get_list_of_providers(self):
-        provider1 = ProviderFactory.create()
-        provider2 = ProviderFactory.create()
-        provider3 = ProviderFactory.create()
-
-        client = APIClient()
-        request = client.get('/api/providers/')
-        response = ProviderListViewSet.list(self, request)
-
-        response_data_keys = []
-        for key, value in response.data[0].items():
-            response_data_keys += [key]
-
+        request = self.factory.get('/api/providers/')
+        response = self.view_set.as_view({'get': 'list'})(request)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response_data_keys, ProviderSerializer.Meta.fields)
+        self.assertEqual(list(response.data[0].values())[1], self.provider1.person.name)
         self.assertEqual(len(response.data), 3)
+     
--- api/views.py
+++ api/views.py
@@ -5,14 +5,6 @@ from customers.models import Provider
 from api.serializers import *
 
 
-class ProviderListViewSet(viewsets.ViewSet):
-    def list(self, request):
-        queryset = Provider.objects.all()
-        serializer = ProviderSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None):
-        queryset = Provider.objects.all()
-        user = get_object_or_404(queryset, pk=pk)
-        serializer = ProviderSerializer(user)
-        return Response(serializer.data)
+class ProviderListViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = ProviderSerializer
+    queryset = Provider.objects.all()
