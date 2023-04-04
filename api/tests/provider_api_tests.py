1812ca8b5cabe53bf22b2955d5493489549577b0
Baraian Camelia <camelia.baraian@rover.com>
2023-04-04 19:03:19 +0300

    split get_request_and_response in 3 methods

--- api/tests/provider_api_tests.py
+++ api/tests/provider_api_tests.py
@@ -10,35 +10,45 @@ from django.test import RequestFactory
 
 class BaseAPITest(APITestCase):
     def setUp(self):
-        self.factory = RequestFactory()
+        self.request_factory = RequestFactory()
         self.view_set = ProviderListViewSet
         self.base_url = '/api/providers/'
 
-    def get_request_and_response(self, id=None):
-        request = self.factory.get(f'{self.base_url}{id}')
-        if id:
-            return self.view_set.as_view({'get': 'retrieve'})(request, pk=id)
+    def get_request(self, id=None):
+        request = self.request_factory.get(f'{self.base_url}{id}')
+        return request
+
+    def get_retrieve_response(self, id=None):
+        request = self.get_request(id)
+        return self.view_set.as_view({'get': 'retrieve'})(request, pk=id)
+
+
+    def get_list_response(self, id=None):
+        request = self.get_request(id)
         return self.view_set.as_view({'get': 'list'})(request)
 
 
 class ProviderApiRetrieveTests(BaseAPITest):
     def test_retrieve_provider(self):
         self.provider = ProviderFactory.create()
-        response = self.get_request_and_response(self.provider.person.id)
+        response = self.get_retrieve_response(self.provider.person.id)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['name'], self.provider.person.name)
 
     def test_return_404_when_id_does_not_exists(self):
-        response = self.get_request_and_response('0')
+        response = self.get_retrieve_response('0')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 
 class ProviderApiListTests(BaseAPITest):
-    def test_get_list_of_providers(self):
+    def setUp(self):
+        super().setUp()
         self.provider1 = ProviderFactory.create()
         self.provider2 = ProviderFactory.create()
         self.provider3 = ProviderFactory.create()
-        response = self.get_request_and_response()
+
+    def test_get_list_of_providers(self):
+        response = self.get_list_response()
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(list(response.data[0].values())[1], self.provider1.person.name)
         self.assertEqual(len(response.data), 3)
