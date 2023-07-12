929a725110626a5f642371982dc308099d47097d
Baraian Camelia <camelia.baraian@rover.com>
2023-07-12 11:31:47 +0300

    delete readonly_files

--- customers/admin.py
+++ customers/admin.py
@@ -6,12 +6,6 @@ from .models import Person, Pet, Provider
 class ProviderInline(admin.TabularInline):
     model =  Provider
 
-    readonly_fields = (
-        "sitter_score",
-        "ratings_score",
-        "overall_rank",
-    )
-
 class PetInline(admin.TabularInline):
     model = Pet
 
