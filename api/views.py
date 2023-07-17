989a439be6707c82228c71f23bfe4b7a0f79c6cb
Baraian Camelia <camelia.baraian@rover.com>
2023-07-17 15:54:51 +0300

    create the filter for provider rating_score

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-from rest_framework.response import Response
+from django.db.models import Q
 
 from api.serializers import *
 from customers.models import Provider
@@ -8,4 +8,14 @@ from customers.models import Provider
 
 class ProviderListViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = ProviderSerializer
-    queryset = Provider.objects.all()
+
+    def get_queryset(self):
+        average_rating_min = self.request.query_params.get("average_rating_min", 3)
+        average_rating_max = self.request.query_params.get("average_rating_max", 5)
+
+        queryset = Provider.objects.all()
+        queryset= queryset.filter(Q(search_score__rating_score__gte=average_rating_min), search_score__rating_score__lte=average_rating_max)
+        return queryset
+      
+
+
